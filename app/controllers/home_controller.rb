5f1729c522be04f827578c785147bb3046e2297b
Tracy Fu <tracy.r.fu@gmail.com>
2015-08-19 23:26:31 -0700

    Try Ruby-LoL and set up CSS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    require 'lol'
+
+    client = Lol::Client.new Rails.application.secrets.lol_api_key, { region: 'na' }
+    @champions = client.static.champion.get(champData: 'image')
   end
 end
