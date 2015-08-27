e6c59db5abe03a1319d56258cdd7ef7d5cfd7ed8
tracyfu <tracy.r.fu@gmail.com>
2015-08-26 23:15:09 -0700

    Add styles to champions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def champion_image_tag(image)
-    image_tag "#{Rails.application.config.cdn}/#{Rails.application.config.version}/img/champion/#{image}", class: 'champion image'
+  def champion_image_tag(image, html_options = {})
+    image_tag "#{Rails.application.config.cdn}/#{Rails.application.config.version}/img/champion/#{image}", class: 'image'
   end
 end
