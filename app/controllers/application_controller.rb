578acf142a4d7c5e7aafe630a26e547f4f73f2a0
tracyfu <tracy.r.fu@gmail.com>
2015-08-26 22:08:05 -0700

    Load champions into the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_filter :get_champions
   protect_from_forgery with: :exception
+
+
+  def get_champions
+    @champions = Champion.includes(:images)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    require 'lol'
-
-    client = Lol::Client.new Rails.application.secrets.lol_api_key, { region: 'na' }
-    @champions = client.static.champion.get(champData: 'image')
   end
 
   def import
