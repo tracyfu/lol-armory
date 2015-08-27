20b58c043966042c1a6a8f7e01f5d5311d4ca5df
tracyfu <tracy.r.fu@gmail.com>
2015-08-26 22:41:45 -0700

    Add a champion image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def champion_image_tag(image)
+    image_tag "#{Rails.application.config.cdn}/#{Rails.application.config.version}/img/champion/#{image}", class: 'champion image'
+  end
 end
