805a422efc6d1d0c0b2f94bda6cd8d7d24ee4eb9
tracyfu <tracy.r.fu@gmail.com>
2015-08-26 22:41:33 -0700

    Add API config vars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_filter :get_champions
   protect_from_forgery with: :exception
 
-
   def get_champions
     @champions = Champion.includes(:images)
   end
--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,13 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module LolArmory
   class Application < Rails::Application
+    config.cdn = 'http://ddragon.leagueoflegends.com/cdn'
+    config.version = '5.16.1'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
