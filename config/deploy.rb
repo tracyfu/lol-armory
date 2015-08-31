0aa82277ccd1b90794992e780f7ed2674771d24e
tracyfu <tracy.r.fu@gmail.com>
2015-08-31 13:35:47 -0700

    Require Capistrano to keep 3 releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
