34319085db1ede703ba93e66f4021ebbbd0b41c6
tracyfu <tracy.r.fu@gmail.com>
2015-09-10 02:08:27 -0700

    Add data for importing item uniques

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.includes(:images).order(:name)
+
+    respond_to do |f|
+      f.html
+      f.json { render json: @items.as_json(only: [:id, :name, :description]) }
+    end
   end
 end
